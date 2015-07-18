24260e459ad75ed130cdc1762b3daba282ac25d5
Chance Russell <chance@chancerussell.io>
2015-07-17 22:49:12 -0500

    update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-quick-om/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject lein-quick-om/lein-template "0.1.0"
+  :description "Simple Leiningen template for Om app development."
+  :url "https://github.com/chancerussell/lein-quick-om"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
